bf5e59219d2dcb04f1e7266717f8c0df0de29151
Mugunthan1417 <231001125@rajalakshmi.edu.in>
2024-06-15 15:49:50 +0530

    Week-11.02.py

--- Week-11/Week-11.02.py
+++ Week-11/Week-11.02.py
@@ -3,8 +3,7 @@ Problem Description:
 Write a Python program that asks the user for their age and prints a message based on the age. Ensure that the program handles cases where the input is not a valid integer.
 
 
-
-Input Format:
+ 
 
 A single line input representing the user's age.
 
